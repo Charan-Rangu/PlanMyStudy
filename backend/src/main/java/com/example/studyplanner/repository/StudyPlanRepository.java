b0c4a79114d2ef8e20c78293577b0882d69e9a94
charan <charanrangu2605@gmail.com>
2026-02-28 13:10:40 +0530

    Delete backend directory

--- backend/src/main/java/com/example/studyplanner/repository/StudyPlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.studyplanner.repository;
-
-import com.example.studyplanner.model.StudyPlan;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudyPlanRepository extends MongoRepository<StudyPlan, String> {
-}
